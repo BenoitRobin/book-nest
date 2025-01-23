0fcf913fe2c88cae667847e07093c14f85e67e73
BenoitRobin <benoitrobin@me.com>
2025-01-23 12:33:39 +0100

    feat: :construction: HeroSection
    create heroSection with html & styling

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import HeroImage from '$assets/hero.png';
+</script>
+
+<section class="default-margin hero">
+	<div class="hero-text">
+		<h1>Book Nest</h1>
+		<h3>Your personal book library.</h3>
+		<h4 class="mt-l">
+			Create your very own digital library where you can keep track of every bokk you own, read and
+			love.
+		</h4>
+		<h4 class="mb-s">
+			Our app offers a beautifully designed, easy-to-use interface that makes managing your book
+			collection a joy.
+		</h4>
+		<button>sign in</button>
+	</div>
+	<img class="hero-image" src={HeroImage} alt="books on a desk" />
+</section>
+
+<style>
+	.hero {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin-top: 100px;
+		margin-bottom: 80px;
+	}
+    .hero-image {
+        width: 40%;
+
+    }
+    .hero-text {
+        display: flex;
+        flex-direction: column;
+        width: 55%;
+        padding-right: 50px;
+    }
+</style>
