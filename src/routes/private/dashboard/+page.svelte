f18fd5014d7746e4fe352cac49f539dee3ae0892
BenoitRobin <benoitrobin@me.com>
2025-01-30 12:08:00 +0100

    feat: :construction: User dashboard html & styling
    User dashboard html & styling

--- src/routes/private/dashboard/+page.svelte
+++ src/routes/private/dashboard/+page.svelte
@@ -1,12 +1,49 @@
 <script lang="ts">
 	import { getUserState } from '$lib/state/user-state.svelte';
+	import Icon from '@iconify/svelte';
 
 	let userContext = getUserState();
-	let { allBooks } = $derived(userContext);
+	let { userName } = $derived(userContext);
 </script>
 
-<ul>
-	{#each allBooks as book}
-		<li>{book.title}</li>
-	{/each}
-</ul>
+<div class="dashboard">
+	<div class="dashboard-header mb-m">
+		<a href="/private/scan-shelf" class="add-book">
+			<Icon icon="icons8:plus" width={'72'} height={'72'} />
+			<p>Add book</p>
+		</a>
+		<div class="headline">
+			<h3 class="bold mb-xs">Welcome back, {userName}.</h3>
+			<p>
+				There's nothing quite like the journey a good book can take you on. Have you discovered any
+				nex favorites recently?
+			</p>
+		</div>
+	</div>
+
+	<!-- BooksCategories -->
+</div>
+
+<style>
+	.dashboard-header {
+		display: flex;
+		justify-content: space-between;
+		align-items: flex-start;
+		width: 100%;
+	}
+
+	.add-book {
+		display: flex;
+		align-items: center;
+		text-decoration: none;
+	}
+
+	.add-book p {
+		margin-left: 8px;
+	}
+	.headline {
+		text-align: right;
+		max-width: 30%;
+		min-width: 300px;
+	}
+</style>
