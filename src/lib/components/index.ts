1268b3e549ca62b1b6b052693eded640034ed672
BenoitRobin <benoitrobin@me.com>
2025-01-30 11:50:27 +0100

    feat: :construction: auth layout html & styling
    auth layout html & styling

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -5,3 +5,4 @@ export {default as HeroSection } from './HeroSection.svelte';
 export {default as Button} from './UI/Button.svelte';
 export {default as Header} from './UI/Header.svelte';
 export {default as AuthForm} from './UI/AuthForm.svelte';
+export {default as SideNavigation} from './UI/SideNavigation.svelte';
